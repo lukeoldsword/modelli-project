a8167dbc620269d311c9ab9acf90845c81810a91
bggroup2018 <bggroup2018@gmail.com>
2017-12-22 11:40:13 +0100

    risolto avoid protected native method

--- webcam-capture/src/main/java/com/github/sarxos/webcam/ds/buildin/natives/Device.java
+++ webcam-capture/src/main/java/com/github/sarxos/webcam/ds/buildin/natives/Device.java
@@ -88,13 +88,13 @@ public final class Device extends CPPObject {
 		return this;
 	}
 
-	public native Pointer<Byte> getName();
+	private native Pointer<Byte> getName();
 
 	public String getNameStr() {
 		return getName().getCString();
 	}
 
-	public native Pointer<Byte> getIdentifier();
+	private native Pointer<Byte> getIdentifier();
 
 	public String getIdentifierStr() {
 		return getIdentifier().getCString();
