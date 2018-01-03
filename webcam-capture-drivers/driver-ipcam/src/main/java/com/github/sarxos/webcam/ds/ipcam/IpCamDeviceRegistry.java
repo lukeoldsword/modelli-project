f9a66dd21c9ba59715bf2cc143e10c03a41c3c39
bggroup2018 <bggroup2018@gmail.com>
2018-01-03 16:43:24 +0100

    prova

--- webcam-capture-drivers/driver-ipcam/src/main/java/com/github/sarxos/webcam/ds/ipcam/IpCamDeviceRegistry.java
+++ webcam-capture-drivers/driver-ipcam/src/main/java/com/github/sarxos/webcam/ds/ipcam/IpCamDeviceRegistry.java
@@ -26,6 +26,7 @@ public class IpCamDeviceRegistry {
 	 * Contains IP cameras.
 	 */
 	private static final List<IpCamDevice> DEVICES = new ArrayList<IpCamDevice>();
+	private static final int MAX_ITEMS = 50;
 
 	/**
 	 * Register IP camera.
@@ -34,19 +35,22 @@ public class IpCamDeviceRegistry {
 	 * @return IP camera device
 	 */
 	public static IpCamDevice register(IpCamDevice ipcam) {
-
-		for (WebcamDevice d : DEVICES) {
-			String name = ipcam.getName();
-			if (d.getName().equals(name)) {
-				throw new WebcamException(String.format("Webcam with name '%s' is already registered", name));
+		if(DEVICES.size() < MAX_ITEMS){
+			
+			for (WebcamDevice d : DEVICES) {
+				String name = ipcam.getName();
+				if (d.getName().equals(name)) {
+					throw new WebcamException(String.format("Webcam with name '%s' is already registered", name));
+				}
 			}
+			
+			DEVICES.add(ipcam);
+			rescan();	
+			return ipcam;
 		}
+		else
+			throw new WebcamException(String.format("Maximum number of webcam reached"));
 
-		DEVICES.add(ipcam);
-
-		rescan();
-
-		return ipcam;
 	}
 
 	public static IpCamDevice register(String name, String url, IpCamMode mode) throws MalformedURLException {
